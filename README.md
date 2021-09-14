a32bde72f2b27bf3bd0bf6e74284d606b9ca0a0f
Richard Niemand <niemand.richard@gmail.com>
2021-09-14 16:52:48 -0600

    feat: added StorageService

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 > git add .
 > npm run commit
 
+Makes use of: https://www.npmjs.com/package/commitizen
--- src/lib/rn-angular-core/rn-angular-core.module.ts
+++ src/lib/rn-angular-core/rn-angular-core.module.ts
@@ -1,16 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { Injectable, ModuleWithProviders, NgModule } from '@angular/core';
 import { DefaultNoComponentGlobalConfig, GlobalConfig, RN_ANGULAR_CORE_CONFIG } from './rn-angular-core.config';
 
 export const DefaultGlobalConfig: GlobalConfig = {
   ...DefaultNoComponentGlobalConfig
 };
 
+@Injectable()
+export class StorageService {
+  setItem = <T>(key: string, value: T): void => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getItem = <TOut>(key: string): TOut => {
+    let rawValue = localStorage.getItem(key);
+    return JSON.parse(rawValue ?? '') as TOut;
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  hasItem(key: string): boolean {
+    return localStorage.hasOwnProperty(key);
+  }
+}
+
 @NgModule({
   imports: [CommonModule],
   declarations: [],
   exports: [],
   entryComponents: [],
+  providers: [StorageService]
 })
 export class RnAngularCoreModule {
   static forRoot(config: Partial<GlobalConfig> = {}): ModuleWithProviders<RnAngularCoreModule> {
