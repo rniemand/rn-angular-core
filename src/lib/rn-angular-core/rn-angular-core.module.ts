9511b1889ba8eb2e275fd6c8e0496240bc1e2d0f
Richard Niemand <niemand.richard@gmail.com>
2021-09-14 17:12:32 -0600

    Moving storage service

--- src/lib/rn-angular-core/rn-angular-core.module.ts
+++ src/lib/rn-angular-core/rn-angular-core.module.ts
@@ -1,30 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Injectable, ModuleWithProviders, NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { DefaultNoComponentGlobalConfig, GlobalConfig, RN_ANGULAR_CORE_CONFIG } from './rn-angular-core.config';
+import { StorageService } from './services/storage.service';
 
 export const DefaultGlobalConfig: GlobalConfig = {
   ...DefaultNoComponentGlobalConfig
 };
-
-@Injectable()
-export class StorageService {
-  setItem = <T>(key: string, value: T): void => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }
-
-  getItem = <TOut>(key: string): TOut => {
-    let rawValue = localStorage.getItem(key);
-    return JSON.parse(rawValue ?? '') as TOut;
-  }
-
-  removeItem(key: string): void {
-    localStorage.removeItem(key);
-  }
-
-  hasItem(key: string): boolean {
-    return localStorage.hasOwnProperty(key);
-  }
-}
 
 @NgModule({
   imports: [CommonModule],
