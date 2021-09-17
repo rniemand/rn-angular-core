3e2291f9d8a19eaa74a3153da5a515cb9e32c023
Richard Niemand <niemand.richard@gmail.com>
2021-09-17 13:18:05 -0600

    Added categories

--- src/lib/rn-angular-core/services/shortcuts.service.ts
+++ src/lib/rn-angular-core/services/shortcuts.service.ts
@@ -22,16 +22,33 @@ export interface SideNavShortcut {
 
 @Injectable()
 export class ShortcutsService {
+  private _shortcuts: { [key:string]: HomeShortcut[] } = {};
   private _homeShortcuts: HomeShortcut[] = [];
   private _sideNavShortcuts: SideNavShortcut[] = [];
 
   // public
   public addHomeShortcut = (shortcut: HomeShortcut) => {
-    this._homeShortcuts.push(shortcut);
+    this.addShortcut(shortcut, 'home');
+  }
+
+  public addShortcut = (shortcut: HomeShortcut, category: string) => {
+    if(!this._shortcuts.hasOwnProperty(category)) {
+      this._shortcuts[category] = [];
+    }
+
+    this._shortcuts[category].push(shortcut);
   }
 
   public getHomeShortcuts = () => {
-    return this._homeShortcuts;
+    return this.getShortcuts('home');
+  }
+
+  public getShortcuts = (category: string) => {
+    if(!this._shortcuts.hasOwnProperty(category)) {
+      return [];
+    }
+
+    return this._shortcuts[category];
   }
 
   public addSideNavShortcut = (shortcut: SideNavShortcut) => {
